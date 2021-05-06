653c1debb926776f461ffced50677f2be393bfc0
vistah <stahlbaum.v@gmail.com>
2021-05-06 15:07:18 +0200

    Aufgabe 2 ueberarbeitet

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,9 @@
+/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(reg => console.log(reg))
         .catch(err => console.log(err));
-}
+}*/
+
+/*Aufgabe 3*/
